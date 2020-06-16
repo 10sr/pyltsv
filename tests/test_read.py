0c83ae8efa1016c3be32780a6012ba86d782843a
10sr <8.slashes@gmail.com>
2020-06-16 14:25:02 +0900

    Add breader test

--- tests/test_read.py
+++ tests/test_read.py
@@ -3,6 +3,7 @@
 
 import unittest
 
+from six import BytesIO
 from six import StringIO
 
 import pyltsv
@@ -16,5 +17,17 @@ class TestReader(unittest.TestCase):
         """Text basic usage of reader."""
         f = StringIO(u"a:1\tb:2\na:3\tb:4")
         ret = list(pyltsv.reader(f))
+        self.assertEqual(len(ret), 2)
+        return
+
+
+class TestBreader(unittest.TestCase):
+    """Test breader."""
+
+    def test_reader(self):
+        # type: () -> None
+        """Text basic usage of reader."""
+        f = BytesIO(b"a:1\tb:2\na:3\tb:4")
+        ret = list(pyltsv.breader(f))
         self.assertEqual(len(ret), 2)
         return
