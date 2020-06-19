9347a001edfe652d2c6af0dc8e3cdd96dfb108c3
10sr <8.slashes@gmail.com>
2020-06-19 17:17:58 +0900

    Add error classes to __init__

--- pyltsv/__init__.py
+++ pyltsv/__init__.py
@@ -5,3 +5,8 @@ from . import read
 breader = read.breader
 reader = read.reader
 ParserConfigError = read.BaseLineParser.ParserConfigError
+ParseError = read.BaseLineParser.ParseError
+EmptyFieldParseError = read.BaseLineParser.EmptyFieldParseError
+LabelOnlyParseError = read.BaseLineParser.LabelOnlyParseError
+InvalidLabelParseError = read.BaseLineParser.InvalidLabelParseError
+InvalidValueParseError = read.BaseLineParser.InvalidValueParseError
