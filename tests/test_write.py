d630ac89179469a06466b079c211644e6c76e061
10sr <8.slashes@gmail.com>
2020-07-07 15:12:58 +0900

    Add more basic writer tests

--- tests/test_write.py
+++ tests/test_write.py
@@ -27,7 +27,8 @@ class TestWriter(unittest.TestCase):
         # type: () -> None
         """Test basic usage of writer."""
         f = StringIO(u"")
-        pyltsv.writer(f).writerow(((u"a", u"1"), (u"b", u"2")))
+        n = pyltsv.writer(f).writerow(((u"a", u"1"), (u"b", u"2")))
+        self.assertEqual(n, 8)
         self.assertEqual(f.getvalue(), u"a:1\tb:2\n")
         return
 
@@ -35,6 +36,27 @@ class TestWriter(unittest.TestCase):
         # type: () -> None
         """Test basic usage of writer."""
         f = StringIO(u"")
-        pyltsv.writer(f).writerow(OrderedDict(((u"a", u"1"), (u"b", u"2"))))
+        n = pyltsv.writer(f).writerow(dict({u"a": u"1", u"b": u"2"}))
+        self.assertEqual(n, 8)
+        self.assertIn(f.getvalue(), (u"a:1\tb:2\n", u"b:2\ta:1\n"))
+        return
+
+    def test_writerow_ordereddict(self):
+        # type: () -> None
+        """Test basic usage of writer."""
+        f = StringIO(u"")
+        n = pyltsv.writer(f).writerow(OrderedDict(((u"a", u"1"), (u"b", u"2"))))
+        self.assertEqual(n, 8)
         self.assertEqual(f.getvalue(), u"a:1\tb:2\n")
+        return
+
+    def test_writerows(self):
+        # type: () -> None
+        """Test basic usage of writer."""
+        row1 = ((u"a", u"1"), (u"b", u"2"))
+        row2 = ((u"a", u"3"), (u"b", u"4"))
+        f = StringIO(u"")
+        n = pyltsv.writer(f).writerows((row1, row2))
+        self.assertEqual(n, 16)
+        self.assertEqual(f.getvalue(), u"a:1\tb:2\na:3\tb:4\n")
         return
